389e2cb4440641f7d60c5d5a430f5089afca43b4
Marten Ringwelski <git@maringuu.de>
2022-11-29 13:11:03 +0100

    tests: Change to work with last commit

--- src/conftest.py
+++ src/conftest.py
@@ -151,8 +151,7 @@ def analysis_plugin(request, monkeypatch, patch_cfg):
     * AnalysisPluginClass: The plugin class type. Must be a class derived from ``AnalysisBasePlugin``.
       The marker has to be set with ``@pytest.mark.with_args`` to work around pytest
       `link weirdness <https://docs.pytest.org/en/7.1.x/example/markers.html#passing-a-callable-to-custom-markers>`.
-    * plugin_start_worker: If set the AnalysisPluginClass.start_worker method will NOT be overwritten.
-      If not set the method is overwritten by a stub that does nothing.
+    * plugin_start_worker: If set the AnalysisPluginClass.start method will be called.
     * plugin_init_kwargs: Additional keyword arguments that shall be passed to the ``AnalysisPluginClass`` constructor
 
     If this fixture does not fit your needs (which normally should not be necessary) you can define a fixture like this:
@@ -170,7 +169,7 @@ def analysis_plugin(request, monkeypatch, patch_cfg):
 
     .. Note::
 
-        If you use the ``plugin_start_worker`` marker and want to modify plugin configuration like for example TIMEOUT
+        If you want to use the ``plugin_start_worker`` marker and want to modify plugin configuration like for example TIMEOUT
         you have to put the following in your test:
 
         .. code-block::
@@ -178,31 +177,16 @@ def analysis_plugin(request, monkeypatch, patch_cfg):
             @pytest.mark.AnalysisPluginClass.with_args(MyFancyPlugin)
             # Don't use `plugin_start_worker`
             def my_fancy_test(analysis_plugin, monkeypatch):
-                # Undo the patching of MyFancyPlugin.start_worker
-                monkeypatch.undo()
                 analysis_plugin.TIMEOUT = 0
-                # Now start the worker
-                analysis_plugin.start_worker()
+                # Now start the worker manually
+                analysis_plugin.start()
     """
-    # IMPORTANT, READ BEFORE EDITING:
-    # This fixture uses the default monkeypatch fixture.
-    # The reason for this is that tests shall be able to undo the patching of `AnalysisPluginClass.start_worker`.
-    # If you want to monkeypatch anything other in this fixture don't use the default monkeypatch fixture but rather
-    # create a new instance.
-    #
-    # See also: The note in the doc comment.
-
     plugin_class_marker = request.node.get_closest_marker('AnalysisPluginClass')
     assert plugin_class_marker, '@pytest.mark.AnalysisPluginClass has to be defined'
     PluginClass = plugin_class_marker.args[0]
     assert issubclass(
         PluginClass, AnalysisBasePlugin
     ), f'{PluginClass.__name__} is not a subclass of {AnalysisBasePlugin.__name__}'
-
-    # We don't want to actually start workers when testing, except for some special cases
-    plugin_start_worker_marker = request.node.get_closest_marker('plugin_start_worker')
-    if not plugin_start_worker_marker:
-        monkeypatch.setattr(PluginClass, 'start_worker', lambda _: None)
 
     plugin_init_kwargs_marker = request.node.get_closest_marker('plugin_init_kwargs')
     kwargs = plugin_init_kwargs_marker.kwargs if plugin_init_kwargs_marker else {}
@@ -211,6 +195,11 @@ def analysis_plugin(request, monkeypatch, patch_cfg):
         view_updater=CommonDatabaseMock(),
         **kwargs,
     )
+
+    # We don't want to actually start workers when testing, except for some special cases
+    plugin_start_worker_marker = request.node.get_closest_marker('plugin_start_worker')
+    if plugin_start_worker_marker:
+        plugin_instance.start()
     yield plugin_instance
 
     plugin_instance.shutdown()
